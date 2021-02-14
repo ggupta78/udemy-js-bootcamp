4ac8415dcf90967a6f227bc13e95d55b513fbbac
Gautam Gupta <ggupta78@gmail.com>
2021-02-14 16:42:47 +0530

    Add api call

--- movies/index.js
+++ movies/index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com', {
+    params: {
+      apikey: 'd6915f',
+      s: 'avengers',
+    },
+  });
+
+  console.log(response);
+};
+
+fetchData();
