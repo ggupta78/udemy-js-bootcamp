c4949c642af2a58f37f36e5fca417b8385500e8b
Gautam Gupta <ggupta78@gmail.com>
2021-03-10 19:12:43 +0530

    Add Chokidar

---
+++ watchit/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const chokidar = require('chokidar');
+
+chokidar
+  .watch('.')
+  .on('add', () => console.log('File added'))
+  .on('change', () => console.log('File changed'))
+  .on('unlink', () => console.log('File unlinked'));
