f2e231decb5d19217f0f9a41bd85405eea9d6e39
Gautam Gupta <ggupta78@gmail.com>
2021-03-11 11:55:36 +0530

    Complete WatchIt

--- watchit/index.js
+++ watchit/index.js
@@ -1,9 +1,39 @@
 #!/usr/bin/env node
 
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
+const chalk = require('chalk');
 
-chokidar
-  .watch('.')
-  .on('add', () => console.log('File added'))
-  .on('change', () => console.log('File changed'))
-  .on('unlink', () => console.log('File unlinked'));
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action(async (args) => {
+    const { filename } = args;
+    const name = filename || 'index.js';
+
+    try {
+      await fs.promises.access(name);
+    } catch (error) {
+      throw new Error('Could not find file: ' + name);
+    }
+
+    let proc;
+    const start = debounce(() => {
+      if (proc) {
+        proc.kill();
+      }
+      console.log(chalk.bold.red('>>>> Starting process <<<<<'));
+      proc = spawn('node', [name], { stdio: 'inherit' });
+    }, 100);
+
+    chokidar
+      .watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
+
+program.parse(process.argv);
