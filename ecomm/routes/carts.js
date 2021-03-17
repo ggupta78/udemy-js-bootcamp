cdd9ff1a09d819d849800b66ee7c6b5f88a10232
Gautam Gupta <ggupta78@gmail.com>
2021-03-17 23:29:30 +0530

    Setup Cart

--- ecomm/routes/carts.js
+++ ecomm/routes/carts.js
@@ -1,10 +1,50 @@
 const express = require('express');
+const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
-router.post('', async (req, res) => {});
+router.post('/cart/products', async (req, res) => {
+  let cart;
+  // Check if cart exists else create
+  if (!req.session.cartId) {
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
 
-router.get('', async (req, res) => {});
+  //Add items to cart
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
+
+  res.send('Product added to cart');
+});
+
+router.get('/cart', async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect('/');
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (const item of cart.items) {
+    item.product = await productsRepo.getOne(item.id);
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
+});
 
 router.post('', async (req, res) => {});
 
