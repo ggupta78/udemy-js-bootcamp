2b7d4cf4782feac788ee2c0141f1ec478c5f112a
Gautam Gupta <ggupta78@gmail.com>
2021-03-19 13:13:27 +0530

    Complete test intro with Mocha

--- hidash/index.test.js
+++ hidash/index.test.js
@@ -1,16 +1,16 @@
 const assert = require('assert');
 const { forEach, map } = require('./index');
 
-const test = (desc, fn) => {
-  console.log('---', desc);
-  try {
-    fn();
-  } catch (error) {
-    console.log(error.message);
-  }
-};
+// const test = (desc, fn) => {
+//   console.log('---', desc);
+//   try {
+//     fn();
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
 
-test('The forEach function', () => {
+it('The forEach function', () => {
   let sum = 0;
   forEach([1, 2, 3], (value) => {
     sum += value;
@@ -19,13 +19,13 @@ test('The forEach function', () => {
   assert.strictEqual(sum, 6, 'forEach Sum failed!');
 });
 
-test('The map function', () => {
+it('The map function', () => {
   const result = map([1, 2, 3], (value) => {
     return value * 2;
   });
 
-  assert.deepStrictEqual(result, [2, 4, 6]);
-  // assert.strictEqual(result[0], 2);
-  // assert.strictEqual(result[1], 4);
-  // assert.strictEqual(result[2], 6);
+  //assert.deepStrictEqual(result, [2, 4, 7]);
+  assert.strictEqual(result[0], 2);
+  assert.strictEqual(result[1], 4);
+  assert.strictEqual(result[2], 6);
 });
